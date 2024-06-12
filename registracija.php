fc127f6668dce96d39270168d09721ffaa86fa70
brlek03 <brlek.jurica@gmail.com>
2024-06-12 20:33:44 +0200

    Added javascript validation

--- registracija.php
+++ registracija.php
@@ -3,7 +3,7 @@ include 'connect.php';
 
 $msg = '';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $ime = $_POST['ime'];
     $prezime = $_POST['prezime'];
     $username = $_POST['username'];
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prijava</title>
+    <title>Registracija</title>
     <link rel="stylesheet" href="style.css">
     <style>
         a {
@@ -82,21 +82,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <main class="container">
         <section class="news">
         <h1>Registracija</h1>
-        <form method="POST" action="registracija.php">
+        <form name="registracija" method="POST" action="registracija.php">
             <label for="ime">Ime:</label>
-            <input type="text" id="ime" name="ime" required>
+            <input type="text" id="ime" name="ime">
+            <span class="error" id="imeError"></span>
             <br>
             <label for="prezime">Prezime:</label>
-            <input type="text" id="prezime" name="prezime" required>
+            <input type="text" id="prezime" name="prezime">
+            <span class="error" id="prezimeError"></span>
             <br>
             <label for="username">Korisničko ime:</label>
-            <input type="text" id="username" name="username" required>
+            <input type="text" id="username" name="username">
+            <span class="error" id="usernameError"></span>
             <br>
             <label for="pass">Lozinka:</label>
-            <input type="password" id="pass" name="pass" required>
+            <input type="password" id="pass" name="pass">
+            <span class="error" id="passError"></span>
             <br>
             <label for="passRep">Ponovite lozinku:</label>
-            <input type="password" id="passRep" name="passRep" required>
+            <input type="password" id="passRep" name="passRep">
+            <span class="error" id="passRepError"></span>
             <br>
             <button type="submit">Registracija</button>
         </form>
@@ -111,4 +116,56 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </footer>
 </body>
+<script type="text/javascript">
+    document.forms['registracija'].addEventListener('submit', function(event){
+        event.preventDefault();
+
+        var uvjeti = true;
+
+        document.querySelectorAll('.error').forEach(el => el.innerText = '');
+        document.querySelectorAll('input').forEach(el => el.style.border = "1px solid #ccc");
+
+        var ime = document.getElementById('ime').value;
+
+        if (ime.length < 2 || ime.length > 20){
+            uvjeti = false;
+            document.getElementById('imeError').innerText = 'Ime mora sadržavati 2 do 20 znakova';
+            document.getElementById("ime").style.border = "1px solid red";
+        };
+
+        var prezime = document.getElementById('prezime').value;
+
+        if (prezime.length < 2 || prezime.length > 35){
+            uvjeti = false;
+            document.getElementById('prezimeError').innerText = 'Prezime mora sadržavati 2 do 35 znakova';
+            document.getElementById("prezime").style.border = "1px solid red";
+        };
+
+        var username = document.getElementById('username').value;
+
+        if (username.length < 5 || username.length > 20){
+            uvjeti = false;
+            document.getElementById('usernameError').innerText = 'Korisničko ime mora sadržavati 5 do 20 znakova';
+            document.getElementById("username").style.border = "1px solid red";
+        };
+
+        var pass = document.getElementById('pass').value;
+
+        if (pass.length < 6 || pass.length > 20){
+            uvjeti = false;
+            document.getElementById('passError').innerText = 'Lozinka mora sadržavati 6 do 20 znakova';
+            document.getElementById("pass").style.border = "1px solid red";
+        };
+
+        var passRep = document.getElementById('passRep').value;
+
+        if (passRep != pass) {
+            uvjeti = false;
+            document.getElementById("passRepError").innerText = 'Lozinka se moraju podudarati';
+            document.getElementById("passRep").style.border = "1px solid red";
+        };
+
+        if (uvjeti) this.submit();
+    });
+</script>
 </html>
